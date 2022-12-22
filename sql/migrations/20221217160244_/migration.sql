f80862bcd845628a77011f1497c6e04e4bef6dfc
auoie <105099407+auoie@users.noreply.github.com>
2022-12-21 18:38:29 -0800

    removed prisma cruft

--- sql/migrations/20221217160244_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropIndex
-DROP INDEX "streams_game_name_at_start_public_sub_only_max_views_id_idx";
-
--- CreateIndex
-CREATE INDEX "streams_game_id_at_start_public_sub_only_max_views_id_idx" ON "streams"("game_id_at_start", "public", "sub_only", "max_views", "id");
--- sql/migrations/20221218050955_/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "streams" ADD COLUMN     "box_art_url_at_start" TEXT DEFAULT '',
-ADD COLUMN     "profile_image_url_at_start" TEXT DEFAULT '';
